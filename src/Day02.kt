5635afacdaa398997ea0a9b2e3537e65f24dfb0a
Ilja Schwarz <ilja.schwarz@rtl.de>
2023-12-01 18:06:25 +0100

    Day 2 prep

---
+++ src/Day02.kt
@@ -0,0 +1,19 @@
+fun main() {
+    fun part1(input: List<String>): Any {
+        var sum = 0
+
+        return sum
+    }
+
+    fun part2(input: List<String>): Any {
+        var sum = 0
+
+        return sum
+    }
+
+
+
+val input = readInput("Day02")
+part1(input).println()
+part2(input).println()
+}
